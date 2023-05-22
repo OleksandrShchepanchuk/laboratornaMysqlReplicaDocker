d272c7367a63e85c970ef60619b963d2200c8c5e
Oleksandr <education.oleksandr@gmail.com>
2023-05-23 00:53:24 +0300

    updated one more time

--- check_replication.sh
+++ check_replication.sh
@@ -13,6 +13,7 @@ Slave_SQL_Running=`mysql -h mysql_slave -u root -p111 -e"show slave status\G"|gr
 
 if [ -z "$Last_IO_Error" ] && [ -z "$Last_SQL_Error" ] && [ -z "$Last_Error" ] &&  [ "$Slave_SQL_Running" = "Yes" ] && [ "$Slave_IO_Running" = "Yes" ]; then
     true
+    echo "Slave is Running"
 else
         echo "Attention: The MySQL slave has stopped its work. As a result, the replication process has been interrupted, and any changes made to the master database will not be reflected in the slave. The possible causes of this issue could be a network connectivity problem, a configuration error, or a problem with the master database. Please investigate the logs and the replication configuration to identify the root cause of the problem. Once the issue is resolved, it is recommended to manually resume the replication process using the START SLAVE command to ensure data consistency between the master and the slave." >> /var/log/check_replication.log
         curl -s --user 'api:9c5860b8473cb726ae70840830e00dd2-81bd92f8-f0ac3521' \
